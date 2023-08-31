3d6c472d973d10783e6cba91b6ef8debcd2a2e70
AnthonyRMatter <134010366+AnthonyRMatter@users.noreply.github.com>
2023-08-31 23:01:39 +0000

    fifth

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -4,56 +4,65 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
 
-    Calculator calculator = new Calculator();
+  Calculator calculator = new Calculator();
 
-    @Test
-    public void testAdd() {
-        int expected = 3;
+  @Test
+  public void testAdd() {
+    int expected = 3;
 
-        int actual = calculator.add(1, 2);
+    int actual = calculator.add(1, 2);
 
-        assertEquals(expected, actual);
-    }
+    assertEquals(expected, actual);
+  }
 
-    @Test
-    public void testSubtract() {
-        int expected = 3;
+  @Test
+  public void testSubtract() {
+    int expected = 3;
 
-        int actual = calculator.subtract(5, 2);
+    int actual = calculator.subtract(5, 2);
 
-        assertEquals(expected, actual);
-    }
+    assertEquals(expected, actual);
+  }
 
-    @Test
-    public void testMultiply() {
-        int expected = 6;
+  @Test
+  public void testMultiply() {
+    int expected = 6;
 
-        int actual = calculator.multiply(2, 3);
+    int actual = calculator.multiply(2, 3);
 
-        assertEquals(expected, actual);
-    }
+    assertEquals(expected, actual);
+  }
 
-    @Test
-    public void testDivide() {
-        int expected = 2;
+  @Test
+  public void testDivide() {
+    int expected = 2;
 
-        int actual = calculator.divide(6, 3);
+    int actual = calculator.divide(6, 3);
 
-        assertEquals(expected, actual);
-    }
+    assertEquals(expected, actual);
+  }
 
-    @Test
-    public void testIsEven() {
-        boolean actual = calculator.isEven(2);
+  @Test
+  public void testIsEven() {
+    boolean actual = calculator.isEven(2);
 
-        assertTrue(actual);
-    }
+    assertTrue(actual);
+  }
 
-    @Test
-    public void testIsOdd() {
-        boolean actual = calculator.isEven(3);
+  @Test
+  public void testIsOdd() {
+    boolean actual = calculator.isEven(3);
 
-        assertFalse(actual);
-    }
+    assertFalse(actual);
+  }
+
+  @Test
+  public void testIncrementArray() {
+    int[] expected = new int[] { 2, 3, 4 };
+
+    int[] actual = calculator.incrementArray(new int[] { 1, 2, 3 });
+
+    assertArrayEquals(expected, actual);
+  }
 
 }
